8d34068308d96fef2e13f275261ccc62f42f1fd1
cuzsor <a0925202613@gmail.com>
2026-03-03 19:28:26 +0800

    Co-authored-by: jimmylu1220 <jimmylu1220@users.noreply.github.com>

--- services/item.go
+++ services/item.go
@@ -1,8 +1,9 @@
 package services
 
 import (
+	"database/sql"
 	"fmt"
-
+	"go-api-practice/database"
 	"go-api-practice/models"
 )
 
@@ -13,13 +14,62 @@ import (
 // GetItems 取得所有項目
 func GetItems() ([]models.Item, error) {
 	// 請實作：查 items 表，回傳列表
-	return nil, fmt.Errorf("請實作：查詢 items 並回傳列表")
+
+	// 1. SQL語法
+	query := "SELECT id, name, description, created_at, updated_at FROM items"
+
+	// 2. 執行SQL
+	rows, err := database.DB.Query(query)
+	if err != nil {
+		return nil, fmt.Errorf("查詢 items 失敗：%w", err)
+	}
+	//查詢結束時關閉連線
+	defer rows.Close()
+
+	// 3. 解析結果
+	var items []models.Item
+	for rows.Next() {
+		var item models.Item
+		if err := rows.Scan(&item.ID, &item.Name, &item.Description, &item.CreatedAt, &item.UpdatedAt); err != nil {
+			return nil, fmt.Errorf("解析 items 失敗：%w", err)
+		}
+		items = append(items, item)
+	}
+
+	// id name description created_at updated_at
+	// 1  物件1 描述        2024-01-01 2024-01-02 <-
+	// 2  物件2 描述        2024-01-03 2024-01-04 <-
+
+	return items, nil
+	//return nil, fmt.Errorf("請實作：查詢 items 並回傳列表")
 }
 
 // GetItemByID 依 ID 取得單一項目
 func GetItemByID(id int) (*models.Item, error) {
 	// 請實作：依 id 查單筆，找不到回傳 ErrNotFound
-	return nil, fmt.Errorf("請實作：依 id 查詢單筆 item（id=%d）", id)
+	// 1
+	var ErrNotFound = fmt.Errorf("record not found")
+
+	query := "SELECT id, name, description, created_at, updated_at FROM items WHERE id = ?"
+
+	var item models.Item
+
+	err := database.DB.QueryRow(query, id).Scan(
+		&item.ID,
+		&item.Name,
+		&item.Description,
+		&item.CreatedAt,
+		&item.UpdatedAt,
+	)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, ErrNotFound
+		}
+		return nil, fmt.Errorf("查詢單筆item失敗:%w", err)
+	}
+
+	return &item, nil
 }
 
 // CreateItem 新增項目
